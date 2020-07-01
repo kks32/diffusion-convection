1abc596ca7d8ca123a5d692458a70d9a7f53d6d1
Krishna Kumar <krishnak@utexas.edu>
2020-06-30 20:48:18 -0500

    :computer: Heat flux

--- diffusion.py
+++ diffusion.py
@@ -8,7 +8,9 @@ from IPython.display import HTML
 # box size, m
 w = h = 1
 # intervals in x-, y- directions, m
-dx = dy = 0.005
+dx = dy = 0.01
+# Thermal conductivity W/(m-K)
+thermal_conductivity = 0.9
 # Thermal diffusivity, m2.s-1
 alphaSoil =  2.08e-7 #2.08e-7 m^2/s
 alphaSteel = 1.172e-5 #m^2/s 1.172e-5 m^2/s
@@ -18,7 +20,7 @@ n = 0.4
 # Viscosity kg/m
 mu = 1.00E-03 
 # Permeability m2
-k = 1E-13
+k = 1E-12
 # Thermal expansion 
 beta = 8.80E-05
 # Cf
@@ -67,8 +69,15 @@ dt = 0.0001 * 3600
 alpha = alphaSoil * np.ones((nx, ny))
 u0 = Tcool * np.ones((nx, ny))
 u = u0.copy()
-
 uu = []
+
+flux = np.zeros((nx, ny))
+fluxx = np.zeros((nx, ny))
+fluxy = np.zeros((nx, ny))
+
+fflux = []
+ffluxx = []
+ffluxy = []
 
 
 # Initial conditions
@@ -91,7 +100,7 @@ for i in range(nx):
             alpha[i,j] = alphaWater
         
 @jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit            
-def do_timestep(u0, u):
+def do_timestep(u0, u, flux, fluxx, fluxy):
     # Propagate with forward-difference in time, central-difference in space, upwind solution for convection
     # Convection velocity term:
     # The velocity corresponds to differential density, since we are measuring the differnetial temp,
@@ -103,6 +112,8 @@ def do_timestep(u0, u):
     dt * (1/(n*mu)*k*g*rhow)*(beta*(u0[1:-1,1:-1]-Tcool)) * \
     (u0[2:,1:-1] - u0[1:-1,1:-1])/(dy)
     '''
+    # Set flux to zero
+    flux = np.zeros((nx, ny))
     for i in range(nx):
         for j in range(ny):
             #mid cable
@@ -111,9 +122,11 @@ def do_timestep(u0, u):
             # left cable
             elif ((i*dx-cx1)**2 + (j*dy-cy1)**2) < cr1r2:
                 u[i,j] = Thot
+
             # right cable
             elif ((i*dx-cx2)**2 + (j*dy-cy2)**2) < cr2r2:
                 u[i,j] = Thot
+
             elif i > 0 and j > 0 and i < (nx -1) and j < (ny -1):
                 # The velocity corresponds to differential density, since we are measuring the differnetial temp,
                 # the rho(1 - beta(T)) is written as rho*(beta*DeltaT)
@@ -124,21 +137,34 @@ def do_timestep(u0, u):
                                            np.minimum(alpha[i, j], alpha[i, j-1]) * (u0[i,j-1] - u0[i,j]))/dx2) + \
                                                         dt * (1/(n*mu)*k*g*rhow)*(beta*(u0[i,j]-Tcool)) * \
                                                         (u0[i+1,j] - u0[i,j])/(dy)
-    
 
+            #Flux around cable
+            # -k * dT/dx
+            if (((i*dx-cx)**2 + (j*dy-cy)**2) < (r2 + dx**2) and ((i*dx-cx)**2 + (j*dy-cy)**2) > (r2 - dx**2)) or (((i*dx-cx1)**2 + (j*dy-cy1)**2) < (cr1r2 + dx**2) and ((i*dx-cx1)**2 + (j*dy-cy1)**2) > (cr1r2 - dx**2)) or  (((i*dx-cx2)**2 + (j*dy-cy2)**2) < (cr2r2 + dx**2) and ((i*dx-cx2)**2 + (j*dy-cy2)**2) > (cr2r2 - dx**2)):
+                fluxx[i,j] = - thermal_conductivity * (u0[i,j-1] - u0[i, j+1])/(2*dx)
+                fluxy[i,j] = - thermal_conductivity * (u0[i-1,j] - u0[i+1, j])/(2*dy)
+                flux[i,j] = np.sqrt(fluxx[i,j]**2 + fluxy[i,j]**2)
 
+            
     u0 = u.copy()
-    return u0, u
+    return u0, u, flux, fluxx, fluxy
 
 # Number of timesteps
-nsteps = 10001
+nsteps = 500001
 npercent = int(nsteps/100)
+id = 0
+fluxt = []
 for m in range(nsteps):
-    if m % (npercent) == 0:
-        print("Completed: {} %".format(m/npercent))
-    u0, u = do_timestep(u0, u)
+    u0, u, flux, fluxx, fluxy = do_timestep(u0, u, flux, fluxx, fluxy)
     if m % (npercent) == 0:
         uu.append(u.copy())
+        fflux.append(flux.copy())
+        ffluxx.append(fluxx.copy())
+        ffluxy.append(fluxy.copy())
+        print("Completed: {} % flux: {} u: {}".format(m/npercent, np.sum(flux[47:54, 42:58]), np.sum(uu[id])))
+        fluxt.append(np.sum(flux[47:54, 42:58]))
+        id = id + 1
+
 
 print("Conduction: ", conduction)
 print("Total simulation time: {} hours".format(dt * nsteps / 3600))
@@ -153,7 +179,8 @@ plt.show()
 heat = u[:,50]
 np.savetxt("heat.csv", heat, delimiter=",")
 
+np.savetxt("flux.csv", fluxt, delimiter=",")
 
-#pcm = plt.pcolormesh(np.flipud(uu[0]))
+#pcm = plt.pcolormesh(np.flipud(fflux[len(fflux)-1]))
 #plt.colorbar()    
 #plt.show()
